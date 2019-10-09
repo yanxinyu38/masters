3ca03ec2403610848a2247452d4c2c195914e8f2
yan <1959516256@qq.com>
2019-10-09 16:57:30 +0800

    HelloWorld.java第二次增加

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
 		System.out.println("I am king");
+		System.out.println("I am your father");
     }
 
 }
