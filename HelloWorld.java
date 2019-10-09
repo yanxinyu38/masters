8cc835816c469cd93bfc6a7022a549227f289a22
yan <1959516256@qq.com>
2019-10-09 16:55:11 +0800

    HelloWorld.java文件增加了一行

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ package com.example.shiro.entity;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+		System.out.println("I am king");
     }
 
 }
